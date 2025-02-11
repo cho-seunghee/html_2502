5a0b2c2fa0a3cd87341a9b65ed019dfaf4e0c5b2
cho-seunghee <nurcle77@gmail.com>
2025-02-11 17:39:04 +0900

    20250211 수정

--- script/top.js
+++ script/top.js
@@ -1,35 +1,49 @@
-jQuery(document).ready(function() {
-    function showAllSubMenu(menuId) {
-        var topMenuItems = document.querySelectorAll('.highDepth');
-        var items = document.querySelectorAll('[id^="lowMenu"]');
-    
-        topMenuItems.forEach(function (item) {
-            var link = item.querySelectorAll('a');
-    
-            link.forEach(function (item) {
-                if (item.id === menuId) {
-                    item
-                        .classList
-                        .add('on');
-                } else {
-                    item
-                        .classList
-                        .remove('on');
-                }
-            });
-        });
-    
-        menuId = 'lowMenu' + menuId;
-    
-        items.forEach(function (item) {
-            if (item.id === menuId) {
-                item.style.display = 'flex';
-                item.style.flexWrap = 'wrap';
-            } else {
-                item.style.display = 'none';
-            }
-        });
-    }
+const searchButton = document.querySelector("nav .desktop-nav .link-search");
+const closeButton = document.querySelector(".search-container .link-close");
+const desktopNav = document.querySelector(".desktop-nav");
+const searchContainer = document.querySelector(".search-container");
+const overlay = document.querySelector(".overlay");
 
-    
+searchButton.addEventListener("click", () => {
+    desktopNav.classList.add("hide");
+    searchContainer.classList.remove("hide");
+    overlay.classList.add("show");
 });
+
+closeButton.addEventListener("click", () => {
+    desktopNav.classList.remove("hide");
+    searchContainer.classList.add("hide");
+    overlay.classList.remove("show");
+});
+
+overlay.addEventListener("click", () => {
+    desktopNav.classList.remove("hide");
+    searchContainer.classList.add("hide");
+    overlay.classList.remove("show");
+});
+
+//Mobile Version
+
+const menuIconContainer = document.querySelector("nav .menu-icon-container");
+const navContainer = document.querySelector(".nav-container");
+
+menuIconContainer.addEventListener("click", () => {
+    navContainer.classList.toggle("active");
+});
+
+const searchBar = document.querySelector(".mobile-search-container .search-bar");
+const nav = document.querySelector(".nav-container nav");
+const searchInput = document.querySelector(".mobile-search-container input");
+const cancelBtn = document.querySelector(".mobile-search-container .cancel-btn");
+
+searchInput.addEventListener("click", () => {
+    searchBar.classList.add("active");
+    nav.classList.add("move-up");
+    desktopNav.classList.add("move-down");
+});
+
+cancelBtn.addEventListener("click", () => {
+    searchBar.classList.remove("active");
+    nav.classList.remove("move-up");
+    desktopNav.classList.remove("move-down");
+})
