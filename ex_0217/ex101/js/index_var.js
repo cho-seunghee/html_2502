4824fb71878a8b4e5e47fdc47f8cd0cd02ef42d4
cho-seunghee <nurcle77@gmail.com>
2025-02-17 14:33:50 +0900

    20250217 수업

---
+++ ex_0217/ex101/js/index_var.js
@@ -0,0 +1,208 @@
+var html;
+
+x = 5;
+y = 6;
+z = x + y;
+
+html = '<pre>';
+html += 'x = 5;<br>';
+html += 'y = 6;<br>';
+html += "z = x + y;<br>";
+html += '"The value of z is: " + z';
+html += '</pre>'
+html += "The value of z is: " + z;
+
+
+document.getElementById("demo1").innerHTML = html;
+
+var x2 = 5;
+var y2 = 6;
+var z2 = x2 + y2;
+
+html = '<pre>';
+html += 'var x2 = 5;<br>';
+html += 'var y2 = 6;<br>';
+html += "var z2 = x2 + y2;<br>";
+html += '"The value of z is: " + z2';
+html += '</pre>'
+html += "The value of z is: " + z2;
+
+document.getElementById("demo2").innerHTML = html;
+
+
+let x3 = 5;
+let y3 = 6;
+let z3 = x3 + y3;
+
+html = '<pre>';
+html += 'let x3 = 5;<br>';
+html += 'let y3 = 6;<br>';
+html += "let z3 = x3 + y3;<br>";
+html += '"The value of z is: " + z3';
+html += '</pre>'
+html += "The value of z is: " + z3;
+
+document.getElementById("demo3").innerHTML = html;
+
+const x4 = 5;
+const y4 = 6;
+const z4 = x4 + y4;
+
+html = '<pre>';
+html += 'const x4 = 5;<br>';
+html += 'const y4 = 6;<br>';
+html += "const z4 = x4 + y4;<br>";
+html += '"The value of z is: " + z4';
+html += '</pre>'
+html += "The value of z is: " + z4;
+
+document.getElementById("demo4").innerHTML = html;
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+
+html = '<pre>';
+html += 'const price1 = 5;<br>';
+html += 'const price2 = 6;<br>';
+html += "let total = price1 + price2;<br>";
+html += 'The total is: " + total';
+html += '</pre>'
+html += "The total is: " + total;
+
+document.getElementById("demo5").innerHTML = html;
+    
+const pi = 3.14;
+let person = "John Doe";
+let answer = 'Yes I am!';
+
+html = '<pre>';
+html += 'const pi = 3.14;<br>';
+html += 'let person = "John Doe";<br>';
+html += "let answer = 'Yes I am!';<br>";
+html += 'pi + "<br>" + person + "<br>" + answer = ';
+html += '</pre>'
+html += pi + "<br>" + person + "<br>" + answer;
+
+document.getElementById("demo6").innerHTML = html;
+
+let carName = "Volvo";
+
+html = '<pre>';
+html += 'let carName = "Volvo";<br>';
+html += 'carName = ';
+html += '</pre>'
+html += carName;
+
+document.getElementById("demo7").innerHTML = html;
+
+let person8 = "John Doe", carName8 = "Volvo", price8 = 200;
+
+html = '<pre>';
+html += 'let person8 = "John Doe", carName8 = "Volvo", price8 = 200;<br>';
+html += 'carName8 = ';
+html += '</pre>'
+html += carName8;
+
+
+document.getElementById("demo8").innerHTML = html;
+
+let person9 = "John Doe",
+carName9 = "Volvo",
+price9 = 200;
+
+html = '<pre>';
+html += 'let person9 = "John Doe",<br>';
+html += 'carName9 = "Volvo",<br>';
+html += 'price9 = 200;<br>';
+html += 'carName9 = ';
+html += '</pre>'
+html += carName9;
+
+document.getElementById("demo9").innerHTML = html;
+
+let carName10;
+
+html = '<pre>';
+html += 'let carName10;<br>';
+html += 'carName10 = ';
+html += '</pre>'
+html += carName10;
+
+document.getElementById("demo11").innerHTML = html;
+
+var carName11 = "Volvo";
+var carName11;
+
+html = '<pre>';
+html += 'var carName11 = "Volvo";<br>';
+html += 'var carName11;<br>';
+html += 'carName11 = ';
+html += '</pre>'
+html += carName11;
+
+document.getElementById("demo11").innerHTML = html;
+
+let x12 = 5 + 2 + 3;
+
+html = '<pre>';
+html += 'let x12 = 5 + 2 + 3;<br>';
+html += 'x12 = ';
+html += '</pre>'
+html += x12;
+
+document.getElementById("demo12").innerHTML = html;
+
+let x13 = "John" + " " + "Doe";
+
+html = '<pre>';
+html += 'let x13 = "John" + " " + "Doe";<br>';
+html += 'x13 = ';
+html += '</pre>'
+html += x13;
+
+document.getElementById("demo13").innerHTML = html;
+
+let x14 = "5" + 2 + 3;
+
+html = '<pre>';
+html += 'let x14 = "5" + 2 + 3;<br>';
+html += 'x14 = ';
+html += '</pre>'
+html += x14;
+
+document.getElementById("demo14").innerHTML = html;
+
+let x15 = 2 + 3 + "5";
+
+html = '<pre>';
+html += 'let x15 = 2 + 3 + "5";<br>';
+html += 'x15 = ';
+html += '</pre>'
+html += x15;
+
+document.getElementById("demo15").innerHTML = html;
+
+let $$$ = 2;
+let $myMoney = 5;
+
+html = '<pre>';
+html += 'let $$$ = 2;<br>';
+html += 'let $myMoney = 5;<br>';
+html += '$$$ + $myMoney = ';
+html += '</pre>';
+html += $$$ + $myMoney;
+
+document.getElementById("demo16").innerHTML = html;
+
+let _x = 2;
+let _100 = 5;
+
+html = '<pre>';
+html += 'let _x = 2;<br>';
+html += 'let _100 = 5;<br>';
+html += '_x + _100 = ';
+html += '</pre>'
+html += _x + _100;
+
+document.getElementById("demo17").innerHTML = html;
