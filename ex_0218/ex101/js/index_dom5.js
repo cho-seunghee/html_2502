1fc1cfda9b034e315fa65d062649e5d207d25baa
cho-seunghee <nurcle77@gmail.com>
2025-02-18 17:29:29 +0900

    20250218 수업

--- ex_0218/ex101/js/index_dom5.js
+++ ex_0218/ex101/js/index_dom5.js
@@ -1,4 +1,4 @@
-const x2 = d.querySelectorAll("p.intro");
+const x2 = d.querySelectorAll("p.intro5");
 
 output = '<div class="output">';
 output += 'class="intro"가 포함된 첫 번째 단락(색인 0)은 다음과 같습니다.: ' + x2[0].innerHTML;
