d465217e218eee7de6cec14848b024540187d063
cho-seunghee <nurcle77@gmail.com>
2025-02-18 17:05:57 +0900

    20250218 수업

--- ex_0218/ex101/js/index_dom6.js
+++ ex_0218/ex101/js/index_dom6.js
@@ -9,3 +9,18 @@ output += text;
 output += '</div>';
 
 d.getElementById("demo6").innerHTML = output;
+
+function fn_submit()
+{
+  text = "";
+
+  for (let i = 0; i < x3.length ;i++) {
+    text += x3.elements[i].value + "<br>";
+  }
+  
+  output = '<div class="output">';
+  output += text;
+  output += '</div>';
+  
+  d.getElementById("demo6").innerHTML = output;
+}
