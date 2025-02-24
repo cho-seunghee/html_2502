c36bb4b2536d118bf1f1e069287b46363d38e9ef
cho-seunghee <nurcle77@gmail.com>
2025-02-24 17:17:07 +0900

    20250224 수업

---
+++ ex_0224/ex101/js/ex_p327.js
@@ -0,0 +1,11 @@
+var d = document;
+var msg = '';
+
+
+fnS9 = () => {
+    document.querySelector(".s9").childNodes[3].childNodes[1].childNodes[3].style.visibility = "hidden";
+}
+
+fnS9_2 = () => {
+    document.querySelector(".s9").childNodes[3].childNodes[1].childNodes[3].style.visibility = "visible";
+}
