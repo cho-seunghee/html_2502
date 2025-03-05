1669f977cf5fd76a4023058dd1ab9ffb1905c92a
cho-seunghee <nurcle77@gmail.com>
2025-03-05 17:35:02 +0900

    20250305 수정

--- ex_0305/ex101/js/index_fn_layout_e.js
+++ ex_0305/ex101/js/index_fn_layout_e.js
@@ -1,3 +1,13 @@
+$(document).ready(function() {
+    $('.content2 img').height($('.content1').height());
+    
+    // 윈도우 리사이즈 시 대응
+    // $(window).resize(function() {
+    //     $('.content2 img').height($('.content1').height());
+    // });
+});
+
+
 $(".menu").hover(function () {
     // over
     $(this).find(".subMenu").stop().fadeIn()
@@ -7,3 +17,10 @@ $(".menu").hover(function () {
     $(".subMenu").stop().fadeOut()
 }
 );
+
+function openPop() {
+    $(".winPopup").show(500);
+}
+function closePop() {
+    $(".winPopup").hide();
+}
