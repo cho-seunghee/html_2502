e941b2cc1958b34b94c2f0d800bc90c60745395c
cho-seunghee <nurcle77@gmail.com>
2025-03-06 17:33:39 +0900

    20250306 수정

--- ex_0305/ex101/js/index_fn_layout_d.js
+++ ex_0305/ex101/js/index_fn_layout_d.js
@@ -1,3 +1,28 @@
+const linkImg = ["./images/g1.jpeg", "./images/g2.jpeg", "./images/g1.jpeg", "./images/g2.jpeg"];
+let linkImgCnt = 0;
+
+$(document).ready(function() {
+    //$('.content2 img').height($('.content1').height());
+    
+
+    if($(".noticeImg").attr('src') != '')
+    {
+        $(".noticeImg").attr('src', linkImg[linkImgCnt]).stop().animate({
+            left : "-100"
+        });
+
+        $(".slideContent img").attr('src', linkImg[linkImgCnt]).fadeIn(3000);
+
+        $(".noticeImg").attr('src', linkImg[linkImgCnt]).stop().animate({
+            left : "0"
+        });
+    }
+    // 윈도우 리사이즈 시 대응
+    // $(window).resize(function() {
+    //     $('.content2 img').height($('.content1').height());
+    // });
+});
+
 $(".menu").hover(function () {
     // over
     $(this)
@@ -11,6 +36,19 @@ $(".menu").hover(function () {
         .stop()
         .slideUp()
 });
+
+$(".noticeNavConR").click(function() {
+    if(linkImgCnt >= linkImg.length) linkImgCnt = 0;
+
+    $(".noticeImg").attr('src', function(){
+        linkImgCnt++;
+        return linkImg[linkImgCnt];
+    });
+
+    $(".slideContent img").attr('src', linkImg[linkImgCnt]);
+    
+});
+
 
 // $('.tabMenu li').click(function() {
 //     var idx = $(this).index();
